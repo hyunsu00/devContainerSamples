7a960d4cebb0757b6897365f16ff36b86496fffd
hyunsu00 <hyun955807@naver.com>
2023-08-15 03:13:02 +0900

    aaa

--- linux-devs/centos7-dev/.devcontainer/reinstall-cmake.sh
+++ linux-devs/centos7-dev/.devcontainer/reinstall-cmake.sh
@@ -27,21 +27,22 @@ trap cleanup EXIT
 
 
 echo "Installing CMake..."
-apt-get -y purge --auto-remove cmake
+yum -y remove cmake
 mkdir -p /opt/cmake
 
-architecture=$(dpkg --print-architecture)
-case "${architecture}" in
-    arm64)
-        ARCH=aarch64 ;;
-    amd64)
-        ARCH=x86_64 ;;
-    *)
-        echo "Unsupported architecture ${architecture}."
-        exit 1
-        ;;
-esac
+# architecture=$(uname -m)
+# case "${architecture}" in
+#     arm64)
+#         ARCH=aarch64 ;;
+#     amd64)
+#         ARCH=x86_64 ;;
+#     *)
+#         echo "Unsupported architecture ${architecture}."
+#         exit 1
+#         ;;
+# esac
 
+ARCH=x86_64
 CMAKE_BINARY_NAME="cmake-${CMAKE_VERSION}-linux-${ARCH}.sh"
 CMAKE_CHECKSUM_NAME="cmake-${CMAKE_VERSION}-SHA-256.txt"
 TMP_DIR=$(mktemp -d -t cmake-XXXXXXXXXX)
@@ -49,11 +50,14 @@ TMP_DIR=$(mktemp -d -t cmake-XXXXXXXXXX)
 echo "${TMP_DIR}"
 cd "${TMP_DIR}"
 
-curl -sSL "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_BINARY_NAME}" -O
-curl -sSL "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_CHECKSUM_NAME}" -O
+# curl -sSL "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_BINARY_NAME}" -O
+# curl -sSL "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_CHECKSUM_NAME}" -O
 
-sha256sum -c --ignore-missing "${CMAKE_CHECKSUM_NAME}"
-sh "${TMP_DIR}/${CMAKE_BINARY_NAME}" --prefix=/opt/cmake --skip-license
+curl -sSL "https://github.com/Kitware/CMake/releases/download/v3.22.2/cmake-3.22.2-linux-x86_64.sh" -O
+curl -sSL "https://github.com/Kitware/CMake/releases/download/v3.22.2/cmake-3.22.2-SHA-256.txt" -O
+
+# sha256sum -c --ignore-missing cmake-3.22.2-SHA-256.txt
+sh "${TMP_DIR}/cmake-3.22.2-linux-x86_64.sh" --prefix=/opt/cmake --skip-license
 
 ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
 ln -s /opt/cmake/bin/ctest /usr/local/bin/ctest
